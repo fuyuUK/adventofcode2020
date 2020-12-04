2d95c8765699b090055b5a1f24079e528739f2a4
A R <59963555+AshOrWhat@users.noreply.github.com>
2020-12-04 13:52:26 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AoCDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = System.IO.File.ReadAllLines(@"C:\Users\harry\Desktop\AdventOfCodeText\Day1.txt");
+            for(int i = 0;i< numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+                        if (int.Parse(numbers[i]) + int.Parse(numbers[j]) + int.Parse(numbers[k]) == 2020)
+                        {
+                            Console.WriteLine(int.Parse(numbers[i]) * int.Parse(numbers[j]) * int.Parse(numbers[k]));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
